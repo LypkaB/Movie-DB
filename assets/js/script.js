6d17309867e5e228076f8383381ec36e14cd6364
LypkaB <lpk.bdn@gmail.com>
2020-08-03 20:00:00 +0300

    Fix bug with empty field

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,13 @@ const searchForm = document.querySelector('#search-form'),
 function apiSearch(e) {
     e.preventDefault();
 
-    const searchText = document.querySelector('.form-control').value,
-          server = 'https://api.themoviedb.org/3/search/multi?api_key=ead41c3eaac089640f31601bd088ab4e&language=en&query=' + searchText;
+    const searchText = document.querySelector('.form-control').value;
+    if (searchText.trim().length === 0) {
+        movies.innerHTML = '<h2 class="col-12 text-center text-danger">The search field must not be empty</h2>';
+        return;
+    }
+
+    const server = 'https://api.themoviedb.org/3/search/multi?api_key=ead41c3eaac089640f31601bd088ab4e&language=en&query=' + searchText;
 
     movies.innerHTML = '<div class="spinner"></div>';
 
@@ -17,13 +22,17 @@ function apiSearch(e) {
         .then((output) => {
             let inner = '';
 
+            if (output.results.length === 0) {
+                inner = '<h2 class="col-12 text-center text-info">No results were found for your search</h2>';
+            }
+
             output.results.forEach((item) => {
                 let nameItem = item.name || item.title;
                 const poster = item.poster_path ? urlPoster + item.poster_path : './assets/images/not-found.jpg';
 
                 inner += `<div class="col-6 col-md-4 col-xl-3 item">
-                            <img class="poster" src="${poster}" alt="${nameItem}">
-                            <h5>${nameItem}</h5>
+                               <img class="poster" src="${poster}" alt="${nameItem}">
+                               <h5>${nameItem}</h5>
                           </div>`;
             });
 
