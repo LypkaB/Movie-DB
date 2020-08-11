cc30d47ef96f77a238d4dc35ceb1dd9085c1e63f
LypkaB <lpk.bdn@gmail.com>
2020-08-11 20:00:00 +0300

    Add page with full info when click on poster

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,11 @@ function apiSearch(e) {
 
     movies.innerHTML = '<div class="spinner"></div>';
 
-    fetch('https://api.themoviedb.org/3/search/multi?api_key=ead41c3eaac089640f31601bd088ab4e&language=en&query=' + searchText)
+    fetch('https://api.themoviedb.org/3/search/multi?api_key=ead41c3eaac089640f31601bd088ab4e&language=en-US&query=' + searchText)
         .then((value) => {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
             return value.json();
         })
         .then((output) => {
@@ -54,15 +57,76 @@ function addEventMedia() {
 
     media.forEach((elem) => {
         elem.style.cursor = 'pointer';
-        elem.addEventListener('click', function() {
-            console.log(this);
-        })
+        elem.addEventListener('click', showFullInfo);
     })
+}
+
+function showFullInfo() {
+    let url = '';
+
+    if (this.dataset.type === 'movie') {
+        url = 'http://api.themoviedb.org/3/movie/' + this.dataset.id + '?api_key=ead41c3eaac089640f31601bd088ab4e&language=en-US';
+    } else if (this.dataset.type === 'tv') {
+        url = 'http://api.themoviedb.org/3/tv/' + this.dataset.id + '?api_key=ead41c3eaac089640f31601bd088ab4e&language=en-US';
+    } else {
+        movies.innerHTML = '<h2 class="col-12 text-center text-danger">An error occured, please try again later</h2>';
+    }
+
+    fetch(url)
+        .then((value) => {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
+            return value.json();
+        })
+        .then((output) => {
+            movies.innerHTML = `<h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+                                <div class="col-4">
+                                    <img src="${urlPoster + output.poster_path}" alt="${output.name || output.title}">
+                                    ${(output.homepage) ?
+                                            `<p class="text-center">
+                                                <a href="${output.homepage}" target="_blank">Official page</a>
+                                            </p>`
+                                        :
+                                            ''
+                                    }
+                                    
+                                    ${(output.imdb_id) ?
+                                            `<p class="text-center">
+                                                <a href="https://imdb.com/title/${output.imdb_id}" target="_blank">Page on IMDB.com</a>
+                                            </p>`
+                                        :
+                                            ''
+                                    }
+                                </div>
+                                <div class="col-8">
+                                    <p>Rating: ${output.vote_average}</p>
+                                    <p>Status: ${output.status}</p>
+                                    <p>Premiere: ${output.first_air_date || output.release_date}</p>
+                                    
+                                    ${(output.last_episode_to_air) ?
+                                            `<p>${output.number_of_seasons} seasons
+                                                ${output.last_episode_to_air.episode_number} episodes released
+                                            </p>`
+                                        :
+                                            ''
+                                    }
+                                    
+                                    <p>Description: ${output.overview}</p>
+                                </div>`;
+        })
+        .catch((reason) => {
+            movies.innerHTML = 'Ooops...something gone wrong';
+            console.log('error: ' + reason.status);
+        });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('https://api.themoviedb.org/3/trending/all/week?api_key=ead41c3eaac089640f31601bd088ab4e')
         .then((value) => {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
             return value.json();
         })
         .then((output) => {
@@ -73,11 +137,10 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             output.results.forEach((item) => {
-                let nameItem = item.name || item.title;
+                let nameItem = item.name || item.title,
+                    mediaType = item.title ? 'movie' : 'tv';
                 const poster = item.poster_path ? urlPoster + item.poster_path : './assets/images/not-found.jpg';
-                let dataInfo = '';
-
-                if (item.media_type !== 'person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+                let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
 
                 inner += `<div class="col-6 col-md-4 col-xl-3 item">
                                <img class="poster" src="${poster}" alt="${nameItem}" ${dataInfo}>
